b523ce6f5654b686b8926e1b41307a4fbee0b95d
Ty <tylippe94@gmail.com>
2023-05-24 10:44:20 -0400

    Commenting out warning code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
-import axios from "axios";
-import { useEffect, useState } from "react";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
 
 export const App = () => {
-  const headers = {
-    "Content-Type": "application/json",
-  };
-  const [welcomeStr, setWelcomeStr] = useState("");
-  const getHello = async () => {
-    const res = await axios.get("http://localhost:3000/", { headers });
-    return res.data;
-  };
+  // const headers = {
+  //   "Content-Type": "application/json",
+  // };
+  // const [welcomeStr, setWelcomeStr] = useState("");
+  // const getHello = async () => {
+  //   const res = await axios.get("http://localhost:3000/", { headers });
+  //   return res.data;
+  // };
 
-  useEffect(() => {
-    (async () => {
-      const res = await getHello();
-      setWelcomeStr(res);
-    })();
-  }, [welcomeStr]);
+  // useEffect(() => {
+  //   (async () => {
+  //     const res = await getHello();
+  //     setWelcomeStr(res);
+  //   })();
+  // }, [welcomeStr]);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{welcomeStr}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
